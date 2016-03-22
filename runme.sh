dc20161c7fbda5d8a1fc8b616189bf963cebdd1c
Gianpaolo Macario <gianpaolo_macario@mentor.com>
2016-03-22 17:03:49 +0100

    Add preparation steps
    Signed-off-by: Gianpaolo Macario <gianpaolo_macario@mentor.com>

--- runme.sh
+++ runme.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+echo "INFO: Install pip"
+curl -O https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+
+echo "INFO: Install AXEMAS"
+pip install axemas
+
+echo "INFO: Create a new AXEMAS project"
+gearbox axemas-quickstart -n ProjectName -p com.company.example
+
 echo "TODO"
 
 # EOF
